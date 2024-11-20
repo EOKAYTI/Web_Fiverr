073dafe8e519c42db1316f7d8794ea64668258aa
tuan <langkhaituan26@gmail.com>
2024-11-20 21:44:59 +0700

    contribute

--- src/templates/HomeTemplate/HomeTemplate.jsx
+++ src/templates/HomeTemplate/HomeTemplate.jsx
@@ -4,13 +4,19 @@ import { Outlet } from "react-router-dom";
 import FooterTemplate from "./components/FooterTemplate";
 import Services from "./components/Services/Services";
 import TryNow from "./components/TryNow/TryNow";
+import Banner from "./components/Banner/Banner";
+import Item from "./components/Item/Item";
+import Video from "./components/Video/Video";
 
 const HomeTemplate = () => {
   return (
     <>
       <HeaderTemplate />
+      <Banner />
+      <Item />
       <Services />
       <TryNow />
+      <Video />
       <Outlet />
       <FooterTemplate />
     </>
---
+++ src/templates/HomeTemplate/components/Item/Item.jsx
@@ -0,0 +1,113 @@
+import React from "react";
+import "./Item.scss";
+const Item = () => {
+  return (
+    <div className="item">
+      <div className="container">
+        <div className="item_group">
+          <div className="item_wrapper">
+            <div className="item_detail">
+              <img
+                src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/programming-tech-thin.56382a2.svg"
+                alt="programming_tech"
+                loading="lazy"
+                class="z0qhg80"
+              ></img>
+              <p>Programming & Tech</p>
+            </div>
+          </div>
+          <div className="item_wrapper">
+            <div className="item_detail">
+              <img
+                src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/graphics-design-thin.ff38893.svg"
+                alt="graphics_design"
+                loading="lazy"
+                class="z0qhg80"
+              />
+              <p>Graphics & Design</p>
+            </div>
+          </div>
+          <div className="item_wrapper">
+            <div className="item_detail">
+              <img
+                src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/digital-marketing-thin.68edb44.svg"
+                alt="digital_marketing"
+                loading="lazy"
+                class="z0qhg80"
+              />
+              <p>Didital Marketing</p>
+            </div>
+          </div>
+          <div className="item_wrapper">
+            <div className="item_detail">
+              <img
+                src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/writing-translation-thin.fd3699b.svg"
+                alt="writing_translation"
+                loading="lazy"
+                class="z0qhg80"
+              />
+              <p>Writing & Translation</p>
+            </div>
+          </div>
+          <div className="item_wrapper">
+            <div className="item_detail">
+              <img
+                src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/video-animation-thin.9d3f24d.svg"
+                alt="video_animation"
+                loading="lazy"
+                class="z0qhg80"
+              />
+              <p>Video & Animation</p>
+            </div>
+          </div>
+          <div className="item_wrapper">
+            <div className="item_detail">
+              <img
+                src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/ai-services-thin.104f389.svg"
+                alt="ai_services"
+                loading="lazy"
+                class="z0qhg80"
+              />
+              <p>AI Service</p>
+            </div>
+          </div>
+          <div className="item_wrapper">
+            <div className="item_detail">
+              <img
+                src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/music-audio-thin.43a9801.svg"
+                alt="music_audio"
+                loading="lazy"
+                class="z0qhg80"
+              />
+              <p>Music & Audio</p>
+            </div>
+          </div>
+          <div className="item_wrapper">
+            <div className="item_detail">
+              <img
+                src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/business-thin.885e68e.svg"
+                alt="business"
+                loading="lazy"
+                class="z0qhg80"
+              />
+              <p>Business</p>
+            </div>
+          </div>
+          <div className="item_wrapper">
+            <div className="item_detail">
+              <img
+                src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/consulting-thin.d5547ff.svg"
+                alt="consulting"
+                loading="lazy"
+                class="z0qhg80"
+              />
+              <p>Consulting</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Item;
