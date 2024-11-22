1c855365cf14ec218cfd02651c145959aba2ac25
tuan <langkhaituan26@gmail.com>
2024-11-22 10:55:22 +0700

    section made on

---
+++ src/templates/HomeTemplate/components/MadeOn/MadeOn.jsx
@@ -0,0 +1,287 @@
+import React from "react";
+import "./madeon.scss";
+const MadeOn = () => {
+  return (
+    <section className="made_on">
+      <div className="container">
+        <h3>Made on Fiverr</h3>
+        <div className="made_on_group">
+          <div className="made_on_img">
+            <div className="overlay">
+              <div className="overlay_icon">
+                <i class="fa-regular fa-heart"></i>
+              </div>
+              <div className="overlay_content">
+                <p>
+                  Featured in: <span>Architecture & Interior Design</span>
+                  <br /> by: <span>canhphuoc</span>
+                </p>
+                <div className="icon">
+                  <i class="fa-solid fa-ellipsis"></i>
+                </div>
+              </div>
+            </div>
+            <img src="./m1.webp" alt="" />
+          </div>
+          <div className="made_on_img">
+            <div className="overlay">
+              <div className="overlay_icon">
+                <i class="fa-regular fa-heart"></i>
+              </div>
+              <div className="overlay_content">
+                <p>
+                  Featured in: <span>Architecture & Interior Design</span>{" "}
+                  <br /> by: <span>canhphuoc</span>
+                </p>
+                <div className="icon">
+                  <i class="fa-solid fa-ellipsis"></i>
+                </div>
+              </div>
+            </div>
+            <img src="./m2.webp" alt="" />
+          </div>
+          <div className="made_on_img">
+            <div className="overlay">
+              <div className="overlay_icon">
+                <i class="fa-regular fa-heart"></i>
+              </div>
+              <div className="overlay_content">
+                <p>
+                  Featured in: <span>Architecture & Interior Design</span>
+                  <br /> by: <span>canhphuoc</span>
+                </p>
+                <div className="icon">
+                  <i class="fa-solid fa-ellipsis"></i>
+                </div>
+              </div>
+            </div>
+            <img src="./m3.webp" alt="" />
+          </div>
+          <div className="made_on_img">
+            <div className="overlay">
+              <div className="overlay_icon">
+                <i class="fa-regular fa-heart"></i>
+              </div>
+              <div className="overlay_content">
+                <p>
+                  Featured in: <span>Architecture & Interior Design</span>
+                  <br /> by: <span>canhphuoc</span>
+                </p>
+                <div className="icon">
+                  <i class="fa-solid fa-ellipsis"></i>
+                </div>
+              </div>
+            </div>
+            <img src="./m4.webp" alt="" />
+          </div>
+          <div className="made_on_img">
+            <div className="overlay">
+              <div className="overlay_icon">
+                <i class="fa-regular fa-heart"></i>
+              </div>
+              <div className="overlay_content">
+                <p>
+                  Featured in: <span>Architecture & Interior Design</span>
+                  <br /> by: <span>canhphuoc</span>
+                </p>
+                <div className="icon">
+                  <i class="fa-solid fa-ellipsis"></i>
+                </div>
+              </div>
+            </div>
+            <img src="./m5.webp" alt="" />
+          </div>
+          <div className="made_on_img">
+            <div className="overlay">
+              <div className="overlay_icon">
+                <i class="fa-regular fa-heart"></i>
+              </div>
+              <div className="overlay_content">
+                <p>
+                  Featured in: <span>Architecture & Interior Design</span>
+                  <br /> by: <span>canhphuoc</span>
+                </p>
+                <div className="icon">
+                  <i class="fa-solid fa-ellipsis"></i>
+                </div>
+              </div>
+            </div>
+            <img src="./m6.webp" alt="" />
+          </div>
+          <div className="made_on_img">
+            <div className="overlay">
+              <div className="overlay_icon">
+                <i class="fa-regular fa-heart"></i>
+              </div>
+              <div className="overlay_content">
+                <p>
+                  Featured in: <span>Architecture & Interior Design</span>
+                  <br /> by: <span>canhphuoc</span>
+                </p>
+                <div className="icon">
+                  <i class="fa-solid fa-ellipsis"></i>
+                </div>
+              </div>
+            </div>
+            <img src="./m7.webp" alt="" />
+          </div>
+          <div className="made_on_img">
+            <div className="overlay">
+              <div className="overlay_icon">
+                <i class="fa-regular fa-heart"></i>
+              </div>
+              <div className="overlay_content">
+                <p>
+                  Featured in: <span>Architecture & Interior Design</span>
+                  <br /> by: <span>canhphuoc</span>
+                </p>
+                <div className="icon">
+                  <i class="fa-solid fa-ellipsis"></i>
+                </div>
+              </div>
+            </div>
+            <img src="./m8.webp" alt="" />
+          </div>
+          <div className="made_on_img">
+            <div className="overlay">
+              <div className="overlay_icon">
+                <i class="fa-regular fa-heart"></i>
+              </div>
+              <div className="overlay_content">
+                <p>
+                  Featured in: <span>Architecture & Interior Design</span>
+                  <br /> by: <span>canhphuoc</span>
+                </p>
+                <div className="icon">
+                  <i class="fa-solid fa-ellipsis"></i>
+                </div>
+              </div>
+            </div>
+            <img src="./m9.webp" alt="" />
+          </div>
+          <div className="made_on_img">
+            <div className="overlay">
+              <div className="overlay_icon">
+                <i class="fa-regular fa-heart"></i>
+              </div>
+              <div className="overlay_content">
+                <p>
+                  Featured in: <span>Architecture & Interior Design</span>
+                  <br /> by: <span>canhphuoc</span>
+                </p>
+                <div className="icon">
+                  <i class="fa-solid fa-ellipsis"></i>
+                </div>
+              </div>
+            </div>
+            <img src="./m10.webp" alt="" />
+          </div>
+          <div className="made_on_img">
+            <div className="overlay">
+              <div className="overlay_icon">
+                <i class="fa-regular fa-heart"></i>
+              </div>
+              <div className="overlay_content">
+                <p>
+                  Featured in: <span>Architecture & Interior Design</span>
+                  <br /> by: <span>canhphuoc</span>
+                </p>
+                <div className="icon">
+                  <i class="fa-solid fa-ellipsis"></i>
+                </div>
+              </div>
+            </div>
+            <img src="./m11.webp" alt="" />
+          </div>
+          <div className="made_on_img">
+            <div className="overlay">
+              <div className="overlay_icon">
+                <i class="fa-regular fa-heart"></i>
+              </div>
+              <div className="overlay_content">
+                <p>
+                  Featured in: <span>Architecture & Interior Design</span>
+                  <br /> by: <span>canhphuoc</span>
+                </p>
+                <div className="icon">
+                  <i class="fa-solid fa-ellipsis"></i>
+                </div>
+              </div>
+            </div>
+            <img src="./m12.webp" alt="" />
+          </div>
+          <div className="made_on_img">
+            <div className="overlay">
+              <div className="overlay_icon">
+                <i class="fa-regular fa-heart"></i>
+              </div>
+              <div className="overlay_content">
+                <p>
+                  Featured in: <span>Architecture & Interior Design</span>
+                  <br /> by: <span>canhphuoc</span>
+                </p>
+                <div className="icon">
+                  <i class="fa-solid fa-ellipsis"></i>
+                </div>
+              </div>
+            </div>
+            <img src="./m13.webp" alt="" />
+          </div>
+          <div className="made_on_img">
+            <div className="overlay">
+              <div className="overlay_icon">
+                <i class="fa-regular fa-heart"></i>
+              </div>
+              <div className="overlay_content">
+                <p>
+                  Featured in: <span>Architecture & Interior Design</span>
+                  <br /> by: <span>canhphuoc</span>
+                </p>
+                <div className="icon">
+                  <i class="fa-solid fa-ellipsis"></i>
+                </div>
+              </div>
+            </div>
+            <img src="./m16.webp" alt="" />
+          </div>
+          <div className="made_on_img">
+            <div className="overlay">
+              <div className="overlay_icon">
+                <i class="fa-regular fa-heart"></i>
+              </div>
+              <div className="overlay_content">
+                <p>
+                  Featured in: <span>Architecture & Interior Design</span>
+                  <br /> by: <span>canhphuoc</span>
+                </p>
+                <div className="icon">
+                  <i class="fa-solid fa-ellipsis"></i>
+                </div>
+              </div>
+            </div>
+            <img src="./m17.webp" alt="" />
+          </div>
+          <div className="made_on_img">
+            <div className="overlay">
+              <div className="overlay_icon">
+                <i class="fa-regular fa-heart"></i>
+              </div>
+              <div className="overlay_content">
+                <p>
+                  Featured in: <span>Architecture & Interior Design</span>
+                  <br /> by: <span>canhphuoc</span>
+                </p>
+                <div className="icon">
+                  <i class="fa-solid fa-ellipsis"></i>
+                </div>
+              </div>
+            </div>
+            <img src="./m8.webp" alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default MadeOn;
