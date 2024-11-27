f20f9cfb510d51c80a2524498e16f8d49233d082
tuan <langkhaituan26@gmail.com>
2024-11-27 22:27:17 +0700

    new api

--- src/services/congViec.service.js
+++ src/services/congViec.service.js
@@ -4,4 +4,7 @@ export const congViecService = {
   getCongViecTheoTen: (keyword) => {
     return http.get(`/cong-viec/lay-danh-sach-cong-viec-theo-ten/${keyword}`);
   },
+  getCongViecTheoChiTietLoai: (keyword) => {
+    return http.get(`/cong-viec/lay-cong-viec-theo-chi-tiet-loai/${keyword}`);
+  },
 };
