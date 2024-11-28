27cab81358df9d621a6c584cbd197eea2527bee1
tuan <langkhaituan26@gmail.com>
2024-11-28 21:42:33 +0700

    show list

--- src/pages/HomeTemplate/components/ListDetail/ListDetail.jsx
+++ src/pages/HomeTemplate/components/ListDetail/ListDetail.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./ListDetail.scss";
+import { congViecService } from "../../../../services/congViec.service";
 
 const ListDetail = () => {
+  useEffect((keyword) => {
+    congViecService
+      .getCongViecTheoChiTietLoai()
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
   return (
     <section className="list_detail">
       <div className="container">
